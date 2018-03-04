dea4401e98c358296de476959c257d6b67cf72ab
weimeilin79 <weimeilin@gmail.com>
2018-03-04 01:52:02 -0500

    add new project

---
+++ registration-command/src/main/java/org/mycompany/Reservation.java
@@ -0,0 +1,35 @@
+package org.mycompany;
+
+public class Reservation {
+	
+	String candidatename;
+	String email;
+	String seatname;
+	String seatno;
+	public String getCandidatename() {
+		return candidatename;
+	}
+	public void setCandidatename(String candidatename) {
+		this.candidatename = candidatename;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSeatname() {
+		return seatname;
+	}
+	public void setSeatname(String seatname) {
+		this.seatname = seatname;
+	}
+	public String getSeatno() {
+		return seatno;
+	}
+	public void setSeatno(String seatno) {
+		this.seatno = seatno;
+	}
+	
+
+}
