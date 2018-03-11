c052ee505411063f942bd468f8166a0e1d9adf9b
weimeilin79 <weimeilin@gmail.com>
2018-03-11 01:46:35 -0500

    Configurable AJAX Rotue

--- seat-ui/app.js
+++ seat-ui/app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const htmls = require('ejs')
+
 const app = express()
 
 app.use(express.static('js'))
-
-
+app.set('view engine', 'ejs');
+ 
 
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
-})
+		
+	 console.log("process.env.HOSTNAME:["+process.env.ROUTE_HOSTNAME+"]");
+   //res.sendFile( __dirname + "/" + "index.html" ,{testenv: process.env.HOSTNAME || 'http://localhost:3000'});
+   
+   res.render("index",{routeurl: process.env.ROUTE_HOSTNAME || 'http://localhost:3000'}) 
+   })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
