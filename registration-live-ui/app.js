09ab56d9d22fd83564cd7e8bca920afe97b8b511
weimeilin79 <weimeilin@gmail.com>
2018-06-06 15:03:33 -0400

    update to port 8080

--- registration-live-ui/app.js
+++ registration-live-ui/app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 	 console.log("process.env.HOSTNAME:["+process.env.ROUTE_HOSTNAME+"]");
    //res.sendFile( __dirname + "/" + "index.html" ,{testenv: process.env.HOSTNAME || 'http://localhost:3000'});
    
-   res.render("index",{routeurl: process.env.ROUTE_HOSTNAME || 'http://localhost:3000'}) 
+   res.render("index",{routeurl: process.env.ROUTE_HOSTNAME || 'http://localhost:8080'}) 
    })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
