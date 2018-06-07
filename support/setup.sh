05a5c3d01f72e54d50f119b8628854bd7e29944c
weimeilin79 <weimeilin@gmail.com>
2018-06-07 01:23:04 -0400

    automate db installation

--- support/setup.sh
+++ support/setup.sh
@@ -4,14 +4,16 @@
 
 
 
-oc login -u system:admin
+#oc login -u system:admin
+oc login https://master.9c4c.openshift.opentlc.com -u opentlc-mgr -p r3dh4t1!
 oc project openshift
-oc create -f fis-image-streams.json
+oc create -f https://raw.githubusercontent.com/jboss-fuse/application-templates/application-templates-2.1.fuse-000085/fis-image-streams.json -n openshift
 oc create -f https://raw.githubusercontent.com/strimzi/strimzi/0.1.0/kafka-inmemory/resources/openshift-template.yaml -n openshift
+oc create -f https://raw.githubusercontent.com/sclorg/nodejs-ex/master/openshift/templates/nodejs.json -n openshift
 
 ############################################################################################
 
-oc login -u developer
+#oc login -u developer
 
 echo "Create new projects"
 oc new-project sko
@@ -20,31 +22,84 @@ echo "Start up AMQ Streaming"
 oc new-app strimzi
 
 
+#DATABSE
+##########################
 echo "Start up MySQL for database access"
-oc create -f https://raw.githubusercontent.com/openshift/origin/master/examples/db-templates/mysql-ephemeral-template.json
+oc create -f mysql-ephemeral-template.json
 oc new-app --template=mysql-ephemeral --param=MYSQL_PASSWORD=password --param=MYSQL_USER=dbuser --param=MYSQL_DATABASE=sampledb
 
-#TODO MANUAL STEP!!!!
 echo "Setup mysqlDB"
-#oc get pods
-#oc rsync db-mysql <mysql_pod_name>:/var/lib/mysql/data
-#oc rsh <mysql_pod>
-#cd /var/lib/mysql/data/db-mysql
-#mysql sampledb --user=dbuser --password < schema.sql
+
+#Wait for mysqldb constainer to startup
+inputchk=`oc get pod | grep -v 'mysql-1-deploy' | grep 'mysql-1' | grep 'Running'`
+while [ -z "$inputchk" ]
+do
+	#do nothing keep loop until mysql is up and running
+	sleep 5s
+	echo .
+	inputchk=`oc get pod | grep -v 'mysql-1-deploy' | grep 'mysql-1' | grep 'Running'`
+done
+
+mysqlpod=`echo $inputchk | awk '{split($0,a," "); print a[1]}'`;
+
+echo $mysqlpod
+
+#Wait for mysqldb to startup
+mysqlconnected=`oc exec $mysqlpod -- bash -c "mysql --user=dbuser --password=password sampledb -e 'select 1'"`
+
+while [ -z "$mysqlconnected" ]
+do
+	#do nothing keep loop until mysql is up and running
+	sleep 5s
+	echo $mysqlconnected
+	mysqlconnected=`oc exec $mysqlpod -- bash -c "mysql --user=dbuser --password=password sampledb -e 'select 1'"`
+done
+
+#Upload schema file
+oc rsync db-mysql $mysqlpod:/var/lib/mysql/data
+
+#Insert table into mysql
+oc exec $mysqlpod -- bash -c "mysql --user=dbuser --password=password sampledb < /var/lib/mysql/data/db-mysql/schema.sql"
+
 
 
 echo "Start up POSTGRESQL for database access"
-oc create -f https://raw.githubusercontent.com/openshift/openshift-ansible/master/roles/openshift_examples/files/examples/v1.3/db-templates/postgresql-ephemeral-template.json
+oc create -f postgresql-ephemeral-template.json
 oc new-app --template=postgresql-ephemeral --param=POSTGRESQL_USER=dbuser --param=POSTGRESQL_PASSWORD=password --param=POSTGRESQL_DATABASE=sampledb
 
 
-#TODO MANUAL STEP!!!!
+
 echo "Setup postgresqkDB"
-#oc get pods
-#oc rsync db-postgresql <postgresql_pod_name>:/var/lib/pgsql/data/userdata
-#oc rsh <mysql_pod>
-#cd /var/lib/pgsql/data/userdata/db-postgresql/
-#psql -U dbuser -d sampledb -a -f schema.sql
+inputchk=`oc get pod | grep -v 'postgresql-1-deploy' | grep 'postgresql-1' | grep 'Running'`
+while [ -z "$inputchk" ]
+do
+	#do nothing keep loop until mysql is up and running
+	sleep 5s
+	echo .
+	inputchk=`oc get pod | grep -v 'postgresql-1-deploy' | grep 'postgresql-1' | grep 'Running'`
+done
+
+postgresqlpod=`echo $inputchk | awk '{split($0,a," "); print a[1]}'`;
+
+echo $postgresqlpod
+
+#Wait for postgresql to startup
+postgresqlconnected=`oc exec $postgresqlpod -- bash -c "psql -U dbuser -q -d sampledb -c 'SELECT 1'"`
+
+while [ -z "$postgresqlconnected" ]
+do
+	#do nothing keep loop until mysql is up and running
+	sleep 5s
+	echo $postgresqlconnected
+	postgresqlconnected=`oc exec $postgresqlpod -- bash -c "psql -U dbuser -q -d sampledb -c 'SELECT 1'"`
+done
+
+#Upload schema file
+oc rsync db-postgresql $postgresqlpod:/var/lib/pgsql/data/userdata
+#Insert table into postgresql
+oc exec $postgresqlpod -- bash -c "psql -U dbuser -d sampledb -a -f /var/lib/pgsql/data/userdata/db-postgresql/schema.sql"
+
+
 
 
 echo "Set up RouteConfigMap"
@@ -73,7 +128,7 @@ oc new-app seat-ui-reader
 
 # SEAT UI
 echo "Install SEAT UI "
-oc new-app --template=nodejs-example --param=NAME=seat-ui --param=SOURCE_REPOSITORY_URL=https://github.com/weimeilin79/sko2018.git --param=CONTEXT_DIR=seat-ui --param=SOURCE_REPOSITORY_REF=master --param=UI_NAME=seat-ui
+oc new-app --template=nodejs-example --param=NAME=seat-ui --param=SOURCE_REPOSITORY_URL=https://github.com/weimeilin79/sko2018.git --param=CONTEXT_DIR=seat-ui --param=SOURCE_REPOSITORY_REF=master
 
 
 ############################################################################################
@@ -84,7 +139,7 @@ oc create -f registration-ui.json
 oc new-app fuse-eap
 
 echo "Install Registration Monitor UI "
-oc new-app --template=nodejs-example --param=NAME=registration-live-ui --param=SOURCE_REPOSITORY_URL=https://github.com/weimeilin79/sko2018.git --param=CONTEXT_DIR=registration-live-ui --param=SOURCE_REPOSITORY_REF=master --param=UI_NAME=registration-live-ui
+oc new-app --template=nodejs-example --param=NAME=registration-live-ui --param=SOURCE_REPOSITORY_URL=https://github.com/weimeilin79/sko2018.git --param=CONTEXT_DIR=registration-live-ui --param=SOURCE_REPOSITORY_REF=master
 
 #Registration Command Center
 echo "Install Registration Command Center "
@@ -98,9 +153,9 @@ oc create -f analytic-listener.yml
 oc new-app analytic-listener
 
 echo "Install Analytic UI"
-oc new-app --template=nodejs-example --param=NAME=analytic-ui --param=SOURCE_REPOSITORY_URL=https://github.com/weimeilin79/sko2018.git --param=CONTEXT_DIR=analytic-ui --param=SOURCE_REPOSITORY_REF=master --param=UI_NAME=analytic-ui
-
-
+oc new-app --template=nodejs-example --param=NAME=analytic-ui --param=SOURCE_REPOSITORY_URL=https://github.com/weimeilin79/sko2018.git --param=CONTEXT_DIR=analytic-ui --param=SOURCE_REPOSITORY_REF=master
+#oc new-app --name nodejs-example nodejs:latest --param=NAME=analytic-ui --param=SOURCE_REPOSITORY_URL=https://github.com/weimeilin79/sko2018.git --param=CONTEXT_DIR=analytic-ui --param=SOURCE_REPOSITORY_REF=master --param=UI_NAME=analytic-ui
+#oc new-app --name analytic-ui nodejs:8~https://github.com/weimeilin79/sko2018/analytic-ui
 ############################################################################################
 
 #Simulator
